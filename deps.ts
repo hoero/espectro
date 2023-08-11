37f1d64203c1646e380bbf8bf2347e74dae5ae65
Hoeru <hoeru@outlook.com>
2023-08-11 13:24:27 -0400

    Fix imports pt1.6

--- deps.ts
+++ deps.ts
@@ -1,3 +1,5 @@
-export * as elmish from 'elmish';
+// Relative import path error if these two come from "imports" in deno.json file when importing as a module in another project, that's why we're using URL imports instead.
+
+export * as elmish from 'https://deno.land/x/elmish@v0.0.4/mod.ts';
 export * as preact from 'preact';
-export * as hooks from 'hooks';
+export * as hooks from 'https://esm.sh/stable/preact@10.13.1/denonext/hooks.js';
